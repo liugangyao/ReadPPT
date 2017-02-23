56c59c8687fdf5e976c68dc6ef2d96f01b804bed
liugangyao <liugangyao@outlook.com>
2017-02-24 07:59:14 +0800

    プロジェクト ファイルを追加します。

---
+++ ReadPPT/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace ReadPPT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(121, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(710, 371);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ReadPPT/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LiugyCommon;
+
+namespace ReadPPT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string folderPath = "";
+            // FolderBrowserDialog の新しいインスタンスを生成する (デザイナから追加している場合は必要ない)
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+
+            // ダイアログの説明を設定する
+            folderBrowserDialog1.Description = "ここに説明を書いてください";
+
+            // ルートになる特殊フォルダを設定する (初期値 SpecialFolder.Desktop)
+            folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer;
+
+            // 初期選択するパスを設定する
+            folderBrowserDialog1.SelectedPath = @"E:\仕事\Project\SRM推進部\00_見積書作成\06_提案書";
+
+            // [新しいフォルダ] ボタンを表示する (初期値 true)
+            folderBrowserDialog1.ShowNewFolderButton = false;
+
+            // ダイアログを表示し、戻り値が [OK] の場合は、選択したディレクトリを表示する
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                folderPath = folderBrowserDialog1.SelectedPath;
+            }
+
+            // 不要になった時点で破棄する (正しくは オブジェクトの破棄を保証する を参照)
+            folderBrowserDialog1.Dispose();
+
+            this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+
+            LiugyPPT.readPPT2txt(folderPath, folderPath);
+
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+
+            MessageBox.Show("END!!!!");
+
+        }
+    }
+}
